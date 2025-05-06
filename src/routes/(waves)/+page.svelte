f893c1658aa179888c38b50e092cacc7656f4bac
v0ncent <83373495+v0ncent@users.noreply.github.com>
2025-05-06 01:01:56 -0500

    content warning section

--- src/routes/(waves)/+page.svelte
+++ src/routes/(waves)/+page.svelte
@@ -2,6 +2,8 @@
 	import NewsSection from '$lib/components/organisms/NewsSection.svelte';
 	import RecentPostings from '$lib/components/organisms/RecentPostings.svelte';
 	import LatestComics from '$lib/components/organisms/LatestComics.svelte';
+	import ContentWarning from '$lib/components/organisms/ContentWarning.svelte';
+	import { onMount } from 'svelte';
 	import type { BlogPost } from '$lib/utils/types';
 
 	export let data: {
@@ -10,50 +12,63 @@
 		comicposts: BlogPost[];
 	};
 
-
 	let { newsposts, galleryandcomicposts, comicposts } = data;
+
+	let showWarning = true;
+
+	onMount(() => {
+		const accepted = localStorage.getItem('content-warning-accepted');
+		if (accepted === 'true') {
+			showWarning = false;
+		}
+	});
+
+	function acceptWarning() {
+		localStorage.setItem('content-warning-accepted', 'true');
+		showWarning = false;
+	}
 </script>
 
-<div class="layout-container">
-	<div class="left-column">
-		{#if newsposts && newsposts.length > 0}
-			<NewsSection {newsposts} />
-			<br />
-		{/if}
+{#if showWarning}
+	<ContentWarning onAccept={acceptWarning} />
+{:else}
+	<div class="layout-container">
+		<div class="left-column">
+			{#if newsposts && newsposts.length > 0}
+				<NewsSection {newsposts} />
+				<br />
+			{/if}
 
-		{#if galleryandcomicposts && galleryandcomicposts.length > 0}
-			<RecentPostings {galleryandcomicposts} />
-		{/if}
-	</div>	
+			{#if galleryandcomicposts && galleryandcomicposts.length > 0}
+				<RecentPostings {galleryandcomicposts} />
+			{/if}
+		</div>
 
-	<div class="right-column">
-		{#if comicposts && comicposts.length > 0}
-			<LatestComics posts={comicposts} />
-		{/if}
+		<div class="right-column">
+			{#if comicposts && comicposts.length > 0}
+				<LatestComics posts={comicposts} />
+			{/if}
+		</div>
 	</div>
-</div>
+{/if}
 
 <style lang="scss">
 	@import '$lib/scss/breakpoints.scss';
+
 	.layout-container {
 		display: grid;
 		grid-template-columns: 2fr 1fr;
 		gap: 32px;
 		padding: 32px;
-
-		max-width: 1200px; 
-		margin: 0 auto; 
+		max-width: 1200px;
+		margin: 0 auto;
 
 		@include for-phone-only {
 			grid-template-columns: 1fr;
 		}
 	}
 
-	.left-column {
-		padding: 16px;
-		border-radius: 8px;
-	}
-
+	.left-column,
 	.right-column {
 		padding: 16px;
 		border-radius: 8px;

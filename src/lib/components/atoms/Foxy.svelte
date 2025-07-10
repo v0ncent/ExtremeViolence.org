4dffd8df46bf85a03648f173046986d37c579b66
v0ncent <83373495+v0ncent@users.noreply.github.com>
2025-07-10 00:16:41 -0500

    bruh

--- src/lib/components/atoms/Foxy.svelte
+++ src/lib/components/atoms/Foxy.svelte
@@ -38,9 +38,11 @@
 </script>
 
 <!-- Clickable pixel (stealth mode, random position) -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="foxy-pixel" on:click={triggerFoxy} style={pixelStyle} />
 
 <!-- Jumpscare overlay -->
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
 {#if showJumpscare}
 	<div class="jumpscare-overlay" on:click={() => (showJumpscare = false)}>
 		<div class="jumpscare-content">
